0a3b1321013e16ab30dca6a2f8f21e6787fc043c
adawsonjc <adawson11@hotmail.co.uk>
2014-03-23 17:30:14 +0000

    .gitattributes 	.gitignore  2DRotationPOC-csharp.sln 	2DRotationPOC.sln 	2DRotationPOC.userprefs 	Assembly-CSharp-vs.csproj 	Assembly-CSharp.csproj 	Library/AnnotationManager 	Library/AssetImportState 	Library/AssetServerCacheV3 	Library/AssetVersioning.db 	Library/BuildPlayer.prefs 	Library/BuildSettings.asset 	Library/CurrentLayout.dwlt 	Library/EditorUserBuildSettings.asset 	Library/EditorUserSettings.asset 	Library/FailedAssetImports.txt 	Library/InspectorExpandedItems.asset 	Library/MonoManager.asset 	Library/ProjectSettings.asset 	Library/ScriptAssemblies/ 	Library/assetDatabase3 	Library/expandedItems 	Library/guidmapper 	Library/metadata/

---
+++ Assets/FloorController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorController : MonoBehaviour {
+
+	//pass in the PLayer Game Object so we can get its current rotation
+	public GameObject player;
+	
+	private Animator animator;
+	
+	void Start () {
+		animator = this.GetComponent<Animator> ();
+	}
+
+	void Update () {
+		float rotation = player.transform.rotation.z;
+		//set the Rotation parameter
+		animator.SetFloat ("Rotation", rotation);
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float rotationSpeed;
+	
+	void Update () {
+		float rotation = Input.GetAxis ("Horizontal") * rotationSpeed;
+		rotation *= Time.deltaTime;
+		// Rotate around our z-axis
+		transform.Rotate (0, 0, -rotation);
+	}
+}
